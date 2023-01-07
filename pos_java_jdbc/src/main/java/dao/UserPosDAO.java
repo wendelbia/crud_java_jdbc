e8cb4d27637114e2cbb80cdd16542eb549f121a5
Wendel da Silva Oliveira <wwwendel@live.com>
2023-01-07 00:31:27 -0200

    criando o buscar todos e buscar por id

--- pos_java_jdbc/src/main/java/dao/UserPosDAO.java
+++ pos_java_jdbc/src/main/java/dao/UserPosDAO.java
@@ -2,35 +2,39 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import conexaojdbc.SingleConnection;
 import model.Userposjava;
 
 public class UserPosDAO {
-	
+
 	private Connection connection;
-	
-	//uso o contrutor para quando chamar esse classe então será injetado o SingleConnection
+
+	// uso o contrutor para quando chamar esse classe então será injetado o
+	// SingleConnection
 	public UserPosDAO() {
 		connection = SingleConnection.getConnection();
 	}
-	
+
 	public void salvar(Userposjava userposjava) {
-		
+
 		try {
 			String sql = "insert into userposjava (id,nome,email) values(?,?,?)";
-			//prepara o sql
+			// prepara o sql
 			PreparedStatement stmt = connection.prepareStatement(sql);
-			//passa os parâmetros de  acordo com as posições
+			// passa os parâmetros de acordo com as posições
 			stmt.setLong(1, userposjava.getId());
 			stmt.setString(2, userposjava.getNome());
 			stmt.setString(3, userposjava.getEmail());
-			//executa
+			// executa
 			stmt.execute();
-			//dá o commit que é salvar no banco
+			// dá o commit que é salvar no banco
 			connection.commit();
-			//vou para a classe de teste
+			// vou para a classe de teste
 		} catch (SQLException e) {
 			try {
 				connection.rollback();
@@ -41,5 +45,41 @@ public class UserPosDAO {
 			e.printStackTrace();
 		}
 	}
-	
+	//retorna uma lista
+	public List<Userposjava> listar() throws SQLException {
+			//instancia a lista
+			List<Userposjava> list = new ArrayList<Userposjava>();
+			//monta o sql
+			String sql = "select * from userposjava";
+			PreparedStatement stmt;
+			stmt = connection.prepareStatement(sql);
+			ResultSet resultado = stmt.executeQuery();
+			//retorna um boolean, enquanto for true
+			while(resultado.next()) {
+				//enquanto tiver dados eu continuo buscando
+				Userposjava userposjava = new Userposjava();
+				//assim vai montando uma lista
+				userposjava.setId(resultado.getLong("id"));
+				userposjava.setNome(resultado.getString("nome"));
+				userposjava.setEmail(resultado.getString("email"));
+				//adiciona na lista
+				list.add(userposjava);
+			}
+			//retorna a lista
+		return list;
+	}
+	//buscando por id
+	public Userposjava buscar(Long id) throws SQLException {
+		//instancio a model
+		Userposjava retorno = new Userposjava();
+		String sql = "select * from userposjava where id = " + id;
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		ResultSet resultado = stmt.executeQuery();
+		while(resultado.next()) {
+			retorno.setId(resultado.getLong("id"));
+			retorno.setNome(resultado.getString("nome"));
+			retorno.setEmail(resultado.getString("email"));
+		}
+		return retorno;	
+	}
 }
--- pos_java_jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos_java_jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -1,5 +1,8 @@
 package pos_java_jdbc.pos_java_jdbc;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import org.junit.Test;
 
 import conexaojdbc.SingleConnection;
@@ -23,12 +26,47 @@ public class TesteBancoJdbc {
 		
 		//o que era estático, agora vamos passar adminâmicamente
 		//aqui converte para long usando o L maiusculo no final
-		userposjava.setId(7L);
-		userposjava.setNome("maria");
-		userposjava.setEmail("maria@live.com");
+		userposjava.setId(8L);
+		userposjava.setNome("ana maria");
+		userposjava.setEmail("ana_maria@live.com");
 		
 		//seto o objeto para ele ter dados
 		userposdao.salvar(userposjava);
-		
+	}
+	
+	@Test
+	public void initListar() throws SQLException {
+		//instancio o UserPosDAO onde tem o método que busca no banco de dados a lista
+		UserPosDAO dao = new UserPosDAO();
+		//crio uma lista do tipo Userposava esse objeto recebe o método que busca a lista salva no banco
+		List<Userposjava> list = dao.listar();
+		//faço um iteração com o for
+		for(Userposjava userposjava:list) {
+			//imprimo
+			System.out.println(userposjava.getId());
+			System.out.println(userposjava.getNome());
+			System.out.println(userposjava.getEmail());
+		}
+	}
+	@Test
+	public void initBuscar() throws SQLException {
+		//instancio o dao
+		UserPosDAO dao = new UserPosDAO();
+		Userposjava userposjava = dao.buscar(6L);
+		System.out.println(userposjava.getId());
+		System.out.println(userposjava.getNome());
+		System.out.println(userposjava.getEmail());
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
